ef620564f5e0e8b8560757a9f8e61e81f7b2bab0
yebin <yebind3308@gmail.com>
2023-09-26 18:26:32 +0900

    [Design] 버튼, 백그라운드 컴포넌트

---
+++ src/components/button/ButtonWrap.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Icon } from "@iconify/react";
+
+const buttonStyle =
+    "flex items-center bg-white px-6 py-2 mx-6 rounded-md shadow hover:opacity-70";
+const textStyle = "font-semibold ml-6 text-xl text-gray-700";
+
+  
+const ButtonWrap = () => {
+    return (
+      <div className="flex items-center py-10">
+        {/* 이력서 버튼 */}
+        <butto n className={buttonStyle}>
+          <Icon icon="bxs:file-pdf" color="#444" width="40" height="40" />
+          <span className={textStyle}>이력서</span>
+        </butto>
+
+        {/* 깃허브 버튼 */}
+        <button
+          className={buttonStyle}
+          onClick={() => window.open("https://github.com/YebinYun", "_blank")}>
+          <Icon className="m-0.5" icon="devicon:git" width="36" height="36" />
+          <span className={textStyle}>깃허브</span>
+        </button>
+
+        {/* 벨로그 버튼 */}
+        <button
+          className={buttonStyle}
+          onClick={() => window.open("https://velog.io/@yebind", "_blank")}>
+          <Icon
+            className="m-1"
+            icon="simple-icons:velog"
+            color="#333"
+            width="32"
+            height="32"
+          />
+          <span className={textStyle}>벨로그</span>
+        </button>
+      </div>
+    );
+};
+
+export default ButtonWrap;
---
+++ src/components/common/Background.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Background = ({ mainclassName, divclassName, children }) => {
+  return (
+    <main
+      className={`w-full h-screen flex justify-center ${mainclassName}`}
+      style={{
+        backgroundImage:
+          "linear-gradient(180deg, rgba(255, 121, 121, 0.68) -34.2%, rgba(255, 102, 102, 0.40) 42.5%, rgba(255, 0, 0, 0.00) 223.53%)",
+      }}>
+      <div
+        className={`w-fit flex flex-col items-center justify-center ${divclassName}`}>
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default Background;
