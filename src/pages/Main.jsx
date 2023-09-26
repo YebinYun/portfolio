d1f79f0a503e1315827cd265e76acbe216939ed0
yebin <yebind3308@gmail.com>
2023-09-26 19:49:58 +0900

    [Feat] 화살표 버튼 애니메이션 적용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,28 +7,29 @@ import ButtonWrap from '../components/button/ButtonWrap';
 const Main = () => {
     return (
       <Background>
-          {/* 인사 문구 */}
-          <div className="w-full flex flex-col items-center">
-            <div className="flex flex-col items-center text-6xl text-white font-extrabold">
-              <span className="py-4">안녕하세요.</span>
-              <span className="py-4">프론트엔드 개발자,</span>
-              <span className="py-4">윤예빈입니다.</span>
-            </div>
-
-            <div className="w-full border-2 border-white my-6" />
-
-            <span className="py-4 text-2xl text-white font-bold">
-              방문해주셔서 감사합니다.
-            </span>
+        {/* 인사 문구 */}
+        <div className="w-full flex flex-col items-center">
+          <div className="flex flex-col items-center text-6xl text-white font-extrabold">
+            <span>안녕하세요.</span>
+            <span className="py-16">프론트엔드 개발자,</span>
+            <span>윤예빈입니다.</span>
           </div>
-        <ButtonWrap/>
-          <Icon
-            icon="iconamoon:arrow-up-2"
-            color="#444"
-            rotate={2}
-            width="50"
-            height="50"
-          />
+
+          <div className="w-full border-2 border-white my-16" />
+
+          <span className="pb-16 text-2xl text-white font-bold">
+            방문해주셔서 감사합니다.
+          </span>
+        </div>
+
+        {/* 사이트버튼 */}
+        <ButtonWrap />
+        {/* 화살표버튼 */}
+        <Icon
+          className={`w-16 h-16 text-gray-600 ${`animate-arrow`}`}
+          icon="iconamoon:arrow-up-2"
+          rotate={2}
+        />
       </Background>
     );
 };
