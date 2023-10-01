af349f40e53ae1fd070d87e81a42dd25b795cc36
yebin <yebind3308@gmail.com>
2023-10-01 20:31:03 +0900

    [Feat] 리스트 생성 및 map 적용, 깃,배포 링크 연결

--- src/main/Project.jsx
+++ src/main/Project.jsx
@@ -9,8 +9,37 @@ import 'swiper/css/pagination';
 // import required modules
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 
+const buttonStyle =
+  "mx-2 p-2 border rounded text-lg font-semibold text-gray-800 shadow hover:text-white hover:bg-red-400 hover:opacity-50";
 
 const Project = ({ nextClick }) => {
+  const projectList = [
+    {
+      title: "삐삐",
+      subTitle: "인테리어 추천 커뮤니티",
+      gitHub: "https://github.com/YebinYun/seb45_main_027",
+      link: "https://bbibbiapp.click/",
+    },
+    {
+      title: "빈진우",
+      subTitle: "운동 트레커_프로젝트",
+      gitHub: "https://github.com/YebinYun/exercise-tracker-app",
+      link: "https://dpftlel21.github.io/exercise-tracker-app/",
+    },
+    {
+      title: "오늘 뭐 먹지 ver. 질문지",
+      subTitle: "메뉴 추천",
+      gitHub: "https://github.com/YebinYun/what-eat-today",
+      link: "https://yebinyun.github.io/what-eat-today/",
+    },
+    {
+      title: "오늘 뭐 먹지 ver. 룰렛",
+      subTitle: "메뉴 추천",
+      gitHub: "https://github.com/YebinYun/lunch-recomandation",
+      link: "https://yebinyun.github.io/lunch-recomandation/",
+    },
+  ];
+
   return (
     <Background divclassName="w-full mx-96">
       <div className="flex flex-col items-center text-white font-extrabold text-shadow-custom mb-20">
@@ -33,18 +62,27 @@ const Project = ({ nextClick }) => {
         pagination={true}
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper">
-        <SwiperSlide className="w-full">
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
+        {projectList.map((list, idx) => (
+          <SwiperSlide key={idx}>
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
+            <div className="bg-white flex flex-col items-center py-12">
+              <h1>{list.title}</h1>
+              <h2>{list.subTitle}</h2>
+              <div className="mt-4">
+                <button
+                  onClick={() => window.open(list.gitHub, "_blank")}
+                  className={buttonStyle}>
+                  GitHub 보러가기
+                </button>
+                <button
+                  onClick={() => window.open(list.link, "_blank")}
+                  className={buttonStyle}>
+                  사이트 보러가기
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
       {/* 마우스 휠 버튼 */}
       <MouseWheel onClick={nextClick} />
