044cedbdefdff09c04b726104ad82ea2badc4ac0
yebin <yebind3308@gmail.com>
2023-10-26 00:04:19 +0900

    [Fix] 스택 레이아웃 수정

--- src/main/Stack.jsx
+++ src/main/Stack.jsx
@@ -4,10 +4,10 @@ import Background from '../components/common/Background';
 import MouseWheel from '../components/button/MouseWheel';
 
 const wrapStyle =
-  "py-8 px-12 rounded-md opacity-80 border";
+  "p-8 rounded-md opacity-80 shadow-lg";
 const h1Style = "w-full text-4xl font-extrabold text-gray-900 text-shadow-custom";
 const divStyle =
-  "hidden lg:flex w-full border-2 my-8 opacity-50 rounded-full shadow";
+  "hidden lg:flex w-full border-2 border-red-200 my-8 opacity-50 rounded-full shadow";
 const ulStyle = "flex w-full mt-6 lg:mt-0 lg:mb-8";
 const iconStyle = "w-8 h-8 mr-2.5";
 const spanStyle = "text-2xl font-semibold text-black text-shadow-custom";
@@ -34,21 +34,21 @@ const skills = {
 const Stack = ({ nextClick }) => {
   return (
     <Background mainclassName="h-screen" divclassName="h-full w-full">
-      <div className="w-full">
+      <div className="w-full mt-96 md:mt-[400px] lg:mt-0">
         <div className="flex justify-center mb-20">
           <h1 className="font-extrabold text-shadow-custom text-6xl mb-4 text-red-300">
             Stack
           </h1>
         </div>
-        <div className="w-full grid grid-rows-3 gap-10 lg:grid-cols-3 lg:grid-rows-none">
+        <div className="w-full grid grid-rows-3 gap-10 lg:grid-cols-3 lg:grid-rows-none ">
           {Object.keys(skills).map((category, index) => (
             <div key={index} className={wrapStyle}>
               <h1 className={h1Style}>{category}</h1>
               <div className={divStyle} />
-              <div className="w-full grid grid-cols-2 lg:flex lg:flex-col">
+              <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:flex lg:flex-col">
                 {skills[category].map((skill, skillIndex) => (
                   <ul key={skillIndex} className={ulStyle}>
-                    <li className="flex">
+                    <li className="flex border-2 border-red-50 bg-red-100 shadow p-4 rounded-lg">
                       <Icon className={iconStyle} icon={skill.icon} />
                       <span className={spanStyle}>{skill.name}</span>
                     </li>
