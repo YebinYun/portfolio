426db94d0f2a87fd41a9db3fdda98dd90609b991
yebin <yebind3308@gmail.com>
2023-11-19 13:40:35 +0900

    fix: props 삭제

--- src/components/common/Background.jsx
+++ src/components/common/Background.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const Background = ({ mainclassName, divclassName, children, customStyle }) => {
+const Background = ({children}) => {
   return (
     <div
-      className={`w-full h-screen ${mainclassName}`}
+      className="w-full h-screen"
       style={{
-        ...customStyle,
         backgroundImage:
           "linear-gradient(180deg, rgb(255, 187, 187) -34.2%, rgba(255, 102, 102, 0.40) 42.5%, rgba(255, 0, 0, 0.00) 223.53%)",
       }}>
